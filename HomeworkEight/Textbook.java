d87fa359c9e4f01860bc734b8d40ff56a9eea323
Andrew <adespinosa14@users.noreply.github.com>
2024-11-10 11:38:27 -0800

    Test

---
+++ HomeworkEight/Textbook.java
@@ -0,0 +1,61 @@
+package HomeworkEight;
+
+public class Textbook 
+{
+	private String SKU;
+	private String title;
+	private int price;
+	private int quantity;
+	
+	public Textbook(String SKU, String title, int price, int quantity) 
+	{
+		this.SKU = SKU;
+		this.title = title;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+// Setters and Getters
+	
+	// Setters
+	public void SetSKU(String SKU) 
+	{
+		this.SKU = SKU;
+	}
+	
+	public void SetTitle(String title) 
+	{
+		this.title = title;
+	}
+	
+	public void SetPrice(int price) 
+	{
+		this.price = price;
+	}
+	
+	public void SetQuantity(int quantity) 
+	{
+	}
+	
+	// Getters
+	public String GetSKU()
+	{
+		return SKU;
+	}
+	
+	public String GetTitle() 
+	{
+		return title;
+	}
+	
+	public int GetPrice() 
+	{
+		return price;
+	}
+	
+	public int GetQuantity() 
+	{
+		return quantity;
+	}
+	
+}
